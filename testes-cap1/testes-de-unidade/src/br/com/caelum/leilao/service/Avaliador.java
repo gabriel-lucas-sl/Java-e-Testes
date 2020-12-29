ab29ff5244516a1a300a204c2cd29ff567f8469c
CAPCO\GSLB <gabriel.silva@capco.com>
2020-12-29 13:44:15 -0300

    "Aula 2 v1"

--- testes-cap1/testes-de-unidade/src/br/com/caelum/leilao/service/Avaliador.java
+++ testes-cap1/testes-de-unidade/src/br/com/caelum/leilao/service/Avaliador.java
@@ -3,6 +3,8 @@ package br.com.caelum.leilao.service;
 import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Avaliador {
@@ -10,12 +12,30 @@ public class Avaliador {
   private double maiorDeTodos = Double.NEGATIVE_INFINITY; // constante que guarda o menor número dentro do Double
 
   private double menorDeTodos = Double.POSITIVE_INFINITY;
+  private List<Lance> maiores;
 
   public void avalia(Leilao leilao) {
+    maiores = new ArrayList<>();
+
     for (Lance lance: leilao.getLances()) {
       if (lance.getValor() > maiorDeTodos) maiorDeTodos = lance.getValor();
       if (lance.getValor() < menorDeTodos) menorDeTodos = lance.getValor();
+      maiores.add(lance);
     }
+
+    Collections.sort(maiores, new Comparator<Lance>() {
+      @Override
+      public int compare(Lance o1, Lance o2) {
+        if (o1.getValor() < o2.getValor()) return 1;
+        if (o1.getValor() > o2.getValor()) return -1;
+        return 0;
+      }
+    });
+    maiores = maiores.subList(0, maiores.size() > 3 ? 3 : maiores.size());
+  }
+
+  public List<Lance> getTresMaiores() {
+    return this.maiores;
   }
 
   public double mediaDosLances(Leilao leilao) {
--- testes-cap1/testes-de-unidade/src/br/com/caelum/leilao/teste/TesteAvaliador.java
+++ testes-cap1/testes-de-unidade/src/br/com/caelum/leilao/teste/TesteAvaliador.java
@@ -1,10 +1,13 @@
 package br.com.caelum.leilao.teste;
 
+import static org.junit.Assert.assertEquals;
+
 import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.dominio.Usuario;
 import br.com.caelum.leilao.service.Avaliador;
 import java.text.DecimalFormat;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -30,8 +33,44 @@ public class TesteAvaliador {
     double maiorEsperado = 400;
     double menorEsperado = 250;
 
-    Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorDeTodos(), 0.00001);
-    Assert.assertEquals(menorEsperado, leiloeiro.getMenorDeTodos(), 0.00001);
+    assertEquals(maiorEsperado, leiloeiro.getMaiorDeTodos(), 0.00001);
+    assertEquals(menorEsperado, leiloeiro.getMenorDeTodos(), 0.00001);
+  }
+
+  @Test
+  public void deveEntenderLeilaoComApenasUmLance() {
+
+    Usuario joao = new Usuario("João");
+    Leilao leilao = new Leilao("Playstation 3");
+    leilao.propoe(new Lance(joao, 1000.00));
+
+    Avaliador leiloeiro = new Avaliador();
+    leiloeiro.avalia(leilao);
+
+    assertEquals(1000.0, leiloeiro.getMaiorDeTodos(), 0.00001);
+    assertEquals(1000.0, leiloeiro.getMenorDeTodos(), 0.00001);
+  }
+
+  @Test
+  public void deveEncontrarOsTresMaioresLances() {
+    Usuario joao = new Usuario("João");
+    Usuario maria = new Usuario("Maria");
+    Leilao leilao = new Leilao("Playstation 3");
+
+    leilao.propoe(new Lance(joao, 100.0));
+    leilao.propoe(new Lance(maria, 200.0));
+    leilao.propoe(new Lance(joao, 300.0));
+    leilao.propoe(new Lance(maria, 400.0));
+
+    Avaliador leiloeiro = new Avaliador();
+    leiloeiro.avalia(leilao);
+
+    List<Lance> maiores = leiloeiro.getTresMaiores();
+    assertEquals(3, maiores.size());
+    assertEquals(400.0, maiores.get(0).getValor(), 0.00001);
+    assertEquals(300.0, maiores.get(1).getValor(), 0.00001);
+    assertEquals(200.0, maiores.get(2).getValor(), 0.00001);
+
   }
 
   @Test
@@ -47,7 +86,7 @@ public class TesteAvaliador {
 
     double mediaEsperada = 316.67;
     DecimalFormat df = new DecimalFormat("###.##");
-    Assert.assertEquals(mediaEsperada,
+    assertEquals(mediaEsperada,
         Double.parseDouble(df.format(new Avaliador().mediaDosLances(leilao))),
         0.00001);
   }
